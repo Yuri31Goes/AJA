50d0802763dbec68cb66aa301675e07632329be9
Yuri Góes <yuridejesusgoes@gmail.com>
2024-10-18 16:45:12 -0300

    Uapdate  18/10/24 16:44PM

--- 01. Tela de Login/Pagamentos/cypress/e2e/LoginMalSucedido.cy.js
+++ 01. Tela de Login/Pagamentos/cypress/e2e/LoginMalSucedido.cy.js
@@ -5,8 +5,32 @@ describe('template spec', () => {
     cy.visit('http://172.16.0.229:8080/aja/login.xhtml')
    
   })
-  it('passes', () => {
-   
+  it('CT6.001 - Insiro CPF Inválido e Senha Válida', () => {
+     
+    Pages.Realizar_Login(1234555555,123456)
   })
+
+  it('CT6.002 - Insiro CPF Inválido e Senha Válida', () => {
+     
+    Pages.Realizar_Login(13809921092,12345678)
+  })
+
+  it('CT6.003 - Campo CPF em branco e Senha Válida', () => {
+     
+    Pages.Realizar_Login(' ','12345678')
+  })
+
+  it('CT6.004 - Campo CPF Válido e Senha em branco ', () => {
+     
+    Pages.Realizar_Login('13809921092',' ')
+  })
+
+  it('CT6.005 - Campo CPF  e Senha em branco ', () => {
+     
+    Pages.Realizar_Login(' ',' ')
+  })
+
+
+
 
 })
--- 01. Tela de Login/Pagamentos/cypress/support/Pages/Login.pages.js
+++ 01. Tela de Login/Pagamentos/cypress/support/Pages/Login.pages.js
@@ -11,4 +11,5 @@ class Pages {
     }
 
 
+
 } export default new Pages()
